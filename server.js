11000df0627710aa527f9d038a9807ddfe4c88f7
Gael Arrambide <gael_arrambide@hotmail.com>
2017-01-31 10:25:14 -0600

    trying heroku

--- server.js
+++ server.js
@@ -60,5 +60,5 @@ app.set('view engine', 'handlebars');
 
 // Init server
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port);
 });
